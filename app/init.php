adb56614c47bb98fe8526c78f60998d03a90fecb
ik-manuel <williamikechukwu@gmail.com>
2022-09-06 03:26:32 +0100

    Restructuring App layout(folder) structure

--- app/init.php
+++ app/init.php
@@ -1,9 +1,9 @@
 <?php
 
 // Load Config 
-require_once 'config.php';
+require_once 'config/config.php';
 
 // Autoload Classes
 spl_autoload_register(function($className){
-    require_once '/' . $className . 'php';
+    require_once 'libraries/' . $className . '.php';
 });
